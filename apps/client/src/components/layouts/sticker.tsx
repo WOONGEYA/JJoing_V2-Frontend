bd4e91a2150026d8f21ac2ac76c8d84b70b8b9c9
lsj0202 <w1004123@gmail.com>
2024-09-03 12:05:42 +0900

    fix: sticker 컴포넌트 프레이머 모션 속성 수정

--- apps/client/src/components/layouts/sticker.tsx
+++ apps/client/src/components/layouts/sticker.tsx
@@ -3,34 +3,22 @@
 import { m } from 'framer-motion';
 import Image from 'next/image';
 
-interface StickerProps {
+type StickerProps = {
   stickerUrl: string;
   width: number;
   height: number;
-}
-
-const initial = {
-  opacity: 0,
-  scale: 0.35,
-};
-
-const animate = {
-  opacity: 1,
-  scale: 1,
-};
-
-const transition = {
-  duration: 0.1,
-  scale: { type: 'spring', damping: 8, stiffness: 100, restDelta: 0.001 },
 };
 
 export const Sticker = ({ stickerUrl, width, height }: StickerProps) => {
   return (
     <m.div
       className="absolute"
-      initial={initial}
-      animate={animate}
-      transition={transition}
+      initial={{ opacity: 0, scale: 0.35 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.1,
+        scale: { type: 'spring', damping: 8, stiffness: 100, restDelta: 0.001 },
+      }}
     >
       <Image alt="Target" src={stickerUrl} width={width} height={height} priority />
     </m.div>
