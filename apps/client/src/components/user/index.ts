889477a847390ba513103635afd70e4e08beac92
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-09-26 14:58:37 +0900

    feat(client): 마이페이지 퍼블리싱

---
+++ apps/client/src/components/user/index.ts
@@ -0,0 +1,3 @@
+export { default as UserContactBox } from './userContactBox';
+export { default as UserProfile } from './userProfile';
+export { default as userProjectContainer } from './userProjectContainer/userProjectContainer';
